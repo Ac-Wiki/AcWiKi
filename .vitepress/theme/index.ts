bbbe72f8eff3ac066407c453d0bf288bcdeda875
conglinyizhi <cslbyivi@outlook.com>
2025-06-02 00:56:46 +0800

    feat(vitepress): 添加页脚组件和布局组件，集成Vercount统计功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,17 +1,16 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+
+import Footer from "../components/Footer.vue";
+import layoutComponent from "../components/Layout.vue";
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: layoutComponent,
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-} satisfies Theme
+    app.component("Footer", Footer);
+  },
+} satisfies Theme;
