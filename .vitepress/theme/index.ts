ef2c20e4f678df84214c2a075cd2aa58f751d4c1
conglinyizhi <cslbyivi@outlook.com>
2025-06-02 02:33:21 +0800

    feat(theme): 添加 Comment 组件到主题配置

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,6 +5,7 @@ import DefaultTheme from "vitepress/theme";
 import "./style.css";
 
 import Footer from "../components/Footer.vue";
+import Comment from "../components/Comment.vue";
 import layoutComponent from "../components/Layout.vue";
 
 export default {
@@ -12,5 +13,6 @@ export default {
   Layout: layoutComponent,
   enhanceApp({ app, router, siteData }) {
     app.component("Footer", Footer);
+    app.component("Commit", Comment);
   },
 } satisfies Theme;
